d4bd0dbd012e75309be355b315c9ae0ffc4e68cb
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-06 06:39:38 -0700

    Add example for finding contact info based on id.

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ Contact.prototype.fullName = function() {
 
 
 
-// // Example:
+// // Example for adding contacts to AddressBook in console:
 // //let addressBook = new AddressBook();
 // let contact = new Contact("Ada", "Lovelace", "503-555-0100");
 // let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
@@ -46,3 +46,9 @@ Contact.prototype.fullName = function() {
 // addressBook.contacts;
 // addressBook.contacts["Ada"];
 // addressBook.contacts["Ada"].phoneNumber
+
+// Example for finding contact info based on entering Contact's id:
+// Run:
+// addressBook.findContact(2);
+// It will return:
+// Contact {firstName: "Grace", lastName: "Hopper", phoneNumber: "503-555-0199", id: 2}
