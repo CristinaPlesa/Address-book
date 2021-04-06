69553fc757deddd1cd01fe2922a05be60abd0cc2
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-06 05:55:43 -0700

    clarify business logic functions by name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-// Business logic
-
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
 }
 
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
